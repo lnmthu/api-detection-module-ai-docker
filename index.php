a340ff9ad7bf5401f8efd9f430a10e8f18194358
minhthu <minhthu512586@gmail.com>
2021-06-17 06:50:29 +0700

    update docker-compose file

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 require_once "./vendor/autoload.php";
 use quynhthu\DetectAttacker\DetectAttacker;
-$detect = $_GET["detect"] ?? "";
-echo ( new DetectAttacker($detect))->handle();
+$text = $_GET["text"] ?? "";
+$result = ( new DetectAttacker($text))->handle();
+$dataObj = new \stdClass();
+$dataObj->text = $text;
+$dataObj->result = $result;
+
+$dataJSON = json_encode($dataObj);
+
+echo $dataJSON;
